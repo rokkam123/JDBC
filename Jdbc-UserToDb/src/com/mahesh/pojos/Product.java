67dcb0db33f3dbba549af05b71539739f5b85835
TL <TL@gmail.com>
2020-04-26 22:15:38 +0530

    Proj 2 UserToDb

---
+++ Jdbc-UserToDb/src/com/mahesh/pojos/Product.java
@@ -0,0 +1,10 @@
+package com.mahesh.pojos;
+
+public class Product {
+// pojo = plane old java object..
+	// changes happens one should not affected another class 
+	
+	public int id;
+	public String productName;
+	
+}
---
+++ Jdbc-UserToDb/src/com/mahesh/pojos/ProductTest.java
@@ -0,0 +1,10 @@
+package com.mahesh.pojos;
+
+public class ProductTest {
+public static void main(String[] args) {
+	Product product=new Product();
+	product.id=1;
+	product.name="Mahesh";
+	System.out.println(product.id +":: "+product.name);
+}
+}
